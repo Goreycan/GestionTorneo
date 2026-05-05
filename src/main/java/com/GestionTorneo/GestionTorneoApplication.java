ef5495ecf23037e8e2ae884552e2229e86568554
Goreycan <goreyc90@gmail.com>
2026-05-05 00:01:58 -0400

    first commit

---
+++ src/main/java/com/GestionTorneo/GestionTorneoApplication.java
@@ -0,0 +1,13 @@
+package com.GestionTorneo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTorneoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTorneoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GestionTorneo/GestionTorneoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GestionTorneo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTorneoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
