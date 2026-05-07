a86e51953e7fef0f62e40acba69542a931f00ba0
kennortega-debug <kenn.ortega@duocuc.cl>
2026-05-07 18:29:08 -0400

    Add files via upload
    Se agrega la tabla local

---
+++ src/main/java/com/GestionTorneo/model/Local.java
@@ -0,0 +1,47 @@
+package com.GestionTorneo.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "locales")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Local {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique=true, nullable=false)
+    private Long id;
+
+    @Column(nullable=false)
+    private String nombre;
+
+    @Column(nullable=false)
+    private String direccion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_comuna", nullable = false)
+    @JsonBackReference//evita la recursividad infinita al serializar a JSON, al identificar la jerarquia de la tabla (tabla hijo)
+    private Comuna comuna;
+
+    @OneToMany(mappedBy="local")
+    @JsonManagedReference//evita la recursividad infinita al serializar a JSON, al  identificar la jerarquia de la tabla (tabla padre)
+    private List<Organizadores> organizadores;
+}
