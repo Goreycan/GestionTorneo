cd12893509c014ed21565ccccc699a31a84e7a44
pafuentesg <pa.fuentesg@duocuc.cl>
2026-05-07 01:26:44 -0400

    Jugador y Mazo (bueno?)

--- src/main/java/com/GestionTorneo/model/Jugador.java
+++ src/main/java/com/GestionTorneo/model/Jugador.java
@@ -1,5 +1,38 @@
 package com.GestionTorneo.model;
 
-public class Jugadores {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+
+@Data
+@NoArgsConstructor 
+@AllArgsConstructor 
+@Builder
+@Entity
+@Table(name = "jugador") 
+
+public class Jugador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+
+    @NotBlank (message = "!crea tu nombre¡ , es obligatorio")
+    @Size(min = 3, max = 100, message = "El nombre debe tener entre 3 y 100 caracteres")
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    
+    @OneToMany(mappedBy = "jugador") //relación para ver en qué torneos ha participado el ugador
+    private List<Participacion> participaciones;
+
+
+    @ManyToOne
+    @JoinColumn(name = "jugador_id", nullable = false)
+    private Jugador jugador; //crea una columna ,que servirá como Llave Foránea ;)
 }
--- src/main/java/com/GestionTorneo/model/Mazo.java
+++ src/main/java/com/GestionTorneo/model/Mazo.java
@@ -1,5 +1,41 @@
 package com.GestionTorneo.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Data
+@NoArgsConstructor 
+@AllArgsConstructor 
+@Builder
+@Entity
+@Table(name = "mazo")
+
 public class Mazo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre del mazo es obligatorio")
+    @Size(min = 3, max = 50, message = "El nombre del mazo debe tener entre 3 y 50 caracteres")
+    @Column(nullable = false, length = 50)
+    private String nombre;
+
+    
+    @ManyToOne
+    @JoinColumn(name = "jugador_id", nullable = false)
+    private Jugador jugador;
+
+    
+    @OneToMany(mappedBy = "mazo") //porsi se repite el mazo??
+    private List<Participacion> participaciones;
+
+    
 }
+
+
+
