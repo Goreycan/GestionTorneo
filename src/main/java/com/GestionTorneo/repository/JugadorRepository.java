438e691ef573861bea049213736643e8df1eb569
pafuentesg <pa.fuentesg@duocuc.cl>
2026-05-07 21:22:23 -0400

    mas orden

--- src/main/java/com/GestionTorneo/repository/JugadorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.GestionTorneo.repository;
-
-import com.GestionTorneo.model.Jugador;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JugadorRepository extends JpaRepository<Jugador, Long> {
-}
